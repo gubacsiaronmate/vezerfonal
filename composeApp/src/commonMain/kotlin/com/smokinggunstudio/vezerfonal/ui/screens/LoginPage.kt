a3f383083e0293eeb5679a6f49d1d5ae2e2d757f
murki05 <baloghmark05@gmail.com>
2025-11-05 13:04:54 +0100

    login page started

--- composeApp/src/commonMain/kotlin/com/smokinggunstudio/vezerfonal/ui/screens/LoginPage.kt
+++ composeApp/src/commonMain/kotlin/com/smokinggunstudio/vezerfonal/ui/screens/LoginPage.kt
@@ -1,27 +1,44 @@
 package com.smokinggunstudio.vezerfonal.ui.screens
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import com.smokinggunstudio.vezerfonal.ui.helpers.ClickEvent
 import com.smokinggunstudio.vezerfonal.ui.state.RegisterState
+import org.jetbrains.compose.resources.stringResource
+import vezerfonal.composeapp.generated.resources.Res
+import vezerfonal.composeapp.generated.resources.login
 
-@Composable fun LoginPage(
+@Composable
+fun LoginPage(
     registerState: RegisterState,
     clickEvent: ClickEvent
-){
-    Column(verticalArrangement = Arrangement.SpaceEvenly,
+) {
+    Column(
+        verticalArrangement = Arrangement.SpaceEvenly,
         horizontalAlignment = Alignment.CenterHorizontally,
         modifier = Modifier.fillMaxSize()
             .background(MaterialTheme.colorScheme.background)
             .padding(horizontal = 8.dp),
         
-    ) {  }
+        ) {
+            Row {
+                Text(
+                    text = stringResource(Res.string.login),
+                    style = MaterialTheme.typography.displayLarge,
+                    color = MaterialTheme.colorScheme.onBackground,
+                    fontWeight = FontWeight.Bold,
+                    textAlign = TextAlign.Start,
+                    maxLines = 1,
+                )
+            }
+        Column(verticalArrangement = Arrangement.SpaceEvenly) {  }
+    }
 }
