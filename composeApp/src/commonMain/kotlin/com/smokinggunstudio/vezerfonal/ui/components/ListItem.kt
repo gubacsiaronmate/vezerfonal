ecb02d624c8b1a6bce784fb4603eafd9b70212be
murki05 <baloghmark05@gmail.com>
2025-11-11 13:59:01 +0100

    Home page list item defined

--- composeApp/src/commonMain/kotlin/com/smokinggunstudio/vezerfonal/ui/components/ListItem.kt
+++ composeApp/src/commonMain/kotlin/com/smokinggunstudio/vezerfonal/ui/components/ListItem.kt
@@ -1,22 +1,21 @@
 package com.smokinggunstudio.vezerfonal.ui.components
 
-import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowRight
 import androidx.compose.material.icons.filled.Person
-import androidx.compose.material3.Card
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import com.smokinggunstudio.vezerfonal.ui.helpers.ClickEvent
-import org.jetbrains.compose.resources.stringResource
-import org.jetbrains.compose.resources.vectorResource
 
 @Composable
 fun ListItem(
@@ -24,26 +23,41 @@ fun ListItem(
     author: String,
     onClick: ClickEvent
 ) {
-    Row(modifier = Modifier
-        .clickable { onClick() }
-        .fillMaxWidth()
-        .padding(8.dp)) {
-        Column(modifier = Modifier
-            .padding(8.dp)
-            .fillMaxWidth()) {
-            Image(imageVector = (Icons.Filled.Person),
-                contentDescription = null)
+    Row(
+        modifier = Modifier
+            .clickable { onClick() }
+            .fillMaxWidth()
+            .padding(16.dp)
+            .background(
+                color = MaterialTheme.colorScheme.surface,
+                shape = RoundedCornerShape(12.dp)
+            ).clickable { onClick() }
+            .padding(16.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Icon(
+            imageVector = Icons.Default.Person,
+            contentDescription = null,
+            modifier = Modifier.size(32.dp)
+        )
+        Spacer(modifier = Modifier.width(12.dp))
+        Column(modifier = Modifier.weight(1f)) {
+            Text(
+                text = title,
+                style = MaterialTheme.typography.bodyLarge,
+                fontWeight = FontWeight.Bold
+            )
+            Text(
+                text = author,
+                style = MaterialTheme.typography.bodyMedium,
+                color = MaterialTheme.colorScheme.onSurface
+            )
         }
-    }
-    Column(modifier = Modifier
-        .padding(8.dp)
-        .fillMaxWidth()) {
-        Text(text = title)
-        Text(text = author)
-    }
-    Column(modifier = Modifier
-        .padding(8.dp)
-        .fillMaxWidth()) {
-        Image(imageVector = (Icons.Filled.ArrowRight), contentDescription = null)
+        
+        Icon(
+            imageVector = Icons.Filled.ArrowRight,
+            contentDescription = null,
+            tint = MaterialTheme.colorScheme.onSurface
+        )
     }
 }
