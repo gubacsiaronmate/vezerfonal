3498978a0f520c8a3276529d3506b33d09391a72
murki05 <baloghmark05@gmail.com>
2025-11-12 13:49:54 +0100

    Filter screen

---
+++ composeApp/src/commonMain/kotlin/com/smokinggunstudio/vezerfonal/ui/components/MessageFilter.kt
@@ -0,0 +1,111 @@
+package com.smokinggunstudio.vezerfonal.ui.components
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Switch
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import org.jetbrains.compose.resources.stringResource
+import org.jetbrains.compose.ui.tooling.preview.Preview
+import vezerfonal.composeapp.generated.resources.Res
+import vezerfonal.composeapp.generated.resources.important
+import vezerfonal.composeapp.generated.resources.search_title_or_in_message_content
+import vezerfonal.composeapp.generated.resources.senders_name
+import vezerfonal.composeapp.generated.resources.time_sent
+import vezerfonal.composeapp.generated.resources.waiting_for_answer
+
+@Composable
+@Preview
+fun MessageFilter() {
+    Column(modifier = Modifier
+        .fillMaxSize()
+        .padding(8.dp)
+        .background(color = MaterialTheme.colorScheme.surface),
+        verticalArrangement = Arrangement.SpaceEvenly
+    ) {
+        OutlinedTextField(
+            value = "",
+            onValueChange = {},
+            label = { Text(
+                text = stringResource(Res.string.senders_name),
+                color = MaterialTheme.colorScheme.onSurface
+            ) },
+            singleLine = true,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 8.dp, vertical = 4.dp)
+        )
+        Column {
+            Text(
+                stringResource(Res.string.time_sent),
+                color = MaterialTheme.colorScheme.onSurface,
+                modifier = Modifier
+                    .padding(horizontal = 8.dp),
+                fontWeight = FontWeight.Bold,
+                fontSize = MaterialTheme.typography.bodyLarge.fontSize
+            )
+            RangeSlider()
+        }
+        Row(modifier = Modifier
+            .fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically) {
+            Text(
+                text = stringResource(Res.string.important),
+                color = MaterialTheme.colorScheme.onSurface,
+                modifier = Modifier
+                    .padding(horizontal = 16.dp),
+                fontWeight = FontWeight.Bold,
+                fontSize = MaterialTheme.typography.bodyLarge.fontSize
+            )
+            Switch(
+                checked = false,
+                onCheckedChange = {},
+                modifier = Modifier.padding(end = 16.dp)
+            )
+        }
+        Row(modifier = Modifier
+            .fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically) {
+            Text(
+                text = stringResource(Res.string.waiting_for_answer),
+                color = MaterialTheme.colorScheme.onSurface,
+                modifier = Modifier
+                    .padding(horizontal = 16.dp),
+                fontWeight = FontWeight.Bold,
+                fontSize = MaterialTheme.typography.bodyLarge.fontSize
+            )
+            Switch(
+                checked = false,
+                onCheckedChange = {},
+                modifier = Modifier.padding(end = 16.dp)
+            )
+        }
+        OutlinedTextField(
+            value = "",
+            onValueChange = {},
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 8.dp, vertical = 4.dp),
+            singleLine = true,
+            label = {
+                Text(
+                    text = stringResource(Res.string.search_title_or_in_message_content),
+                    color = MaterialTheme.colorScheme.onSurface
+                )
+            }
+        )
+    }
+}
---
+++ composeApp/src/commonMain/kotlin/com/smokinggunstudio/vezerfonal/ui/components/RangeSlider.kt
@@ -0,0 +1,27 @@
+package com.smokinggunstudio.vezerfonal.ui.components
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.RangeSlider
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import org.jetbrains.compose.ui.tooling.preview.Preview
+
+@Preview
+@Composable
+fun RangeSlider() {
+    var sliderPosition by remember { mutableStateOf(0f..100f) }
+    Column {
+        RangeSlider(
+            value = sliderPosition,
+            steps = 5,
+            onValueChange = { range -> sliderPosition = range },
+            valueRange = 0f..100f,
+            onValueChangeFinished = { },
+        )
+        Text(text = sliderPosition.toString())
+    }
+}
