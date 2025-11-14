93c8f4e2f29585370d0b2ebe3b8b20e16a586ab4
murki05 <baloghmark05@gmail.com>
2025-11-14 13:47:31 +0100

    import changes

--- composeApp/src/commonMain/kotlin/com/smokinggunstudio/vezerfonal/ui/screens/SettingsScreen.kt
+++ composeApp/src/commonMain/kotlin/com/smokinggunstudio/vezerfonal/ui/screens/SettingsScreen.kt
@@ -1,14 +1,7 @@
 package com.smokinggunstudio.vezerfonal.ui.screens
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowRight
 import androidx.compose.material.icons.filled.Archive
