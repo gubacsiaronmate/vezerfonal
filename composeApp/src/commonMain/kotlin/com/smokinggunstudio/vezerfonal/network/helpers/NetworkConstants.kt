e54731248c5aee2aff10b483aa1fb0c29bf75609
garon <21.gubacsi.aron.mate@mechwart.com>
2025-11-19 11:23:58 +0100

    docs

--- composeApp/src/commonMain/kotlin/com/smokinggunstudio/vezerfonal/network/helpers/NetworkConstants.kt
+++ composeApp/src/commonMain/kotlin/com/smokinggunstudio/vezerfonal/network/helpers/NetworkConstants.kt
@@ -1,16 +1,20 @@
 package com.smokinggunstudio.vezerfonal.network.helpers
 
 import com.smokinggunstudio.vezerfonal.helpers.TokenResponse
+import com.smokinggunstudio.vezerfonal.data.MessageData
+import io.ktor.http.HttpStatusCode
 
 object NetworkConstants {
     const val BASE_URL = "https://api.vezerfonal.org"
     
     object Endpoints {
-        // POST:  ->
-        const val REGISTER_DATA_BASIC = "/register/basic" // POST
-        /** POST:/register/basic/pfp/{userId}/{rememberMe} -> [TokenResponse] */
+        /** POST: /register/basic -> success: [HttpStatusCode.Created] + id: [Int] */
+        const val REGISTER_DATA_BASIC = "/register/basic"
+        /** POST: /register/basic/pfp/{userId}/{rememberMe} -> tokens: [TokenResponse] */
         const val REGISTER_PICTURE = "/register/basic/pfp/"
-        const val LOGIN_BASIC = "/login/basic" // POST
-        const val GET_MESSAGES = "/api/messages/" // GET:/api/messages/{amount}
+        /** POST: /login/basic -> tokens: [TokenResponse] */
+        const val LOGIN_BASIC = "/login/basic"
+        /** GET: /api/messages/{amount} -> messages: [List]<[MessageData]> */
+        const val GET_MESSAGES = "/api/messages/"
     }
 }
