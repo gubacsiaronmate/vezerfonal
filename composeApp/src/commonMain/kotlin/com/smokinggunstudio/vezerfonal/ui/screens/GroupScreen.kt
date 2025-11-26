dcd708e54f0d0d3d1e96c26da631e3171630e7e7
murki05 <baloghmark05@gmail.com>
2025-11-26 11:58:30 +0100

    import

--- composeApp/src/commonMain/kotlin/com/smokinggunstudio/vezerfonal/ui/screens/GroupScreen.kt
+++ composeApp/src/commonMain/kotlin/com/smokinggunstudio/vezerfonal/ui/screens/GroupScreen.kt
@@ -1,17 +1,12 @@
 package com.smokinggunstudio.vezerfonal.ui.screens
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import com.smokinggunstudio.vezerfonal.ui.components.GroupCard
 import org.jetbrains.compose.resources.stringResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
 import vezerfonal.composeapp.generated.resources.Res
