bb278000e59a8e44d272c3f32ede9dd41e610717
murki05 <baloghmark05@gmail.com>
2025-11-26 11:57:51 +0100

    group screen

---
+++ composeApp/src/commonMain/kotlin/com/smokinggunstudio/vezerfonal/ui/components/GroupCard.kt
@@ -0,0 +1,46 @@
+package com.smokinggunstudio.vezerfonal.ui.components
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowRight
+import androidx.compose.material.icons.filled.Groups2
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.unit.dp
+import org.jetbrains.compose.ui.tooling.preview.Preview
+
+@Preview
+@Composable
+fun GroupCard(
+) {
+    Row(modifier = Modifier
+        .fillMaxWidth()
+        .background(MaterialTheme.colorScheme.surface)
+        .padding(16.dp),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceBetween)
+    {
+        Row(verticalAlignment = Alignment.CenterVertically) {
+            Image(
+                imageVector = Icons.Filled.Groups2,
+                contentDescription = null,
+                colorFilter = ColorFilter.tint(MaterialTheme.colorScheme.onSurface)
+            )
+            Text(
+                text = "Group Name",
+                color = MaterialTheme.colorScheme.onSurface,
+                modifier = Modifier
+                    .padding(start = 16.dp)
+            )
+        }
+        Image(imageVector = Icons.AutoMirrored.Filled.ArrowRight,
+            contentDescription = null,
+            colorFilter = ColorFilter.tint(MaterialTheme.colorScheme.onSurfaceVariant))
+    }
+}
--- composeApp/src/commonMain/kotlin/com/smokinggunstudio/vezerfonal/ui/screens/GroupScreen.kt
+++ composeApp/src/commonMain/kotlin/com/smokinggunstudio/vezerfonal/ui/screens/GroupScreen.kt
@@ -1,5 +1,37 @@
 package com.smokinggunstudio.vezerfonal.ui.screens
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.smokinggunstudio.vezerfonal.ui.components.GroupCard
+import org.jetbrains.compose.resources.stringResource
+import org.jetbrains.compose.ui.tooling.preview.Preview
+import vezerfonal.composeapp.generated.resources.Res
+import vezerfonal.composeapp.generated.resources.groups
 
-@Composable fun GroupScreen() {}
+@Preview(showBackground = true)
+@Composable fun GroupScreen() {
+    Column(modifier = Modifier
+        .fillMaxSize()
+        .padding(8.dp)
+        .background(color = MaterialTheme.colorScheme.surface)) {
+        Text(
+            text = stringResource(Res.string.groups),
+            style = MaterialTheme.typography.headlineSmall,
+            color = MaterialTheme.colorScheme.onSurface,
+            modifier = Modifier
+                .padding(8.dp)
+        )
+        Spacer(modifier = Modifier
+            .height(16.dp))
+        
+    }
+}
