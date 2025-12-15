534c189c40888abe8696d99ecea808d25b6b28d3
murki05 <baloghmark05@gmail.com>
2025-12-15 10:40:45 +0100

    snackbars added for testing purposes

--- composeApp/src/commonMain/kotlin/com/smokinggunstudio/vezerfonal/ui/screens/ChangePasswordScreen.kt
+++ composeApp/src/commonMain/kotlin/com/smokinggunstudio/vezerfonal/ui/screens/ChangePasswordScreen.kt
@@ -10,9 +10,11 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import com.smokinggunstudio.vezerfonal.ui.components.DismissibleSnackBar
 import com.smokinggunstudio.vezerfonal.ui.state.ChangePasswordState
 import org.jetbrains.compose.resources.stringResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
@@ -21,73 +23,79 @@ import vezerfonal.composeapp.generated.resources.*
 @Composable
 fun ChangePasswordScreen() {
     val changePasswordState by remember { mutableStateOf(ChangePasswordState()) }
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(16.dp)
-            .background(color = MaterialTheme.colorScheme.surface)
-    ) {
-        Text(
-            text = stringResource(Res.string.change_password),
-            style = MaterialTheme.typography.headlineSmall
-        )
+    var isSnackBarVisible by remember { mutableStateOf(false) }
+    Box{
         Column(
-            modifier = Modifier.fillMaxWidth()
-                .fillMaxHeight(0.6f),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.SpaceEvenly
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp)
+                .background(color = MaterialTheme.colorScheme.surface)
         ) {
-            OutlinedTextField(
-                value = changePasswordState.currentPassword,
-                onValueChange = changePasswordState::updateCurrentPassword,
-                label = {
-                    Text(
-                        stringResource(Res.string.old_password),
-                        color = MaterialTheme.colorScheme.onSurface
-                    )
-                },
-                singleLine = true,
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(horizontal = 8.dp, vertical = 4.dp)
+            Text(
+                text = stringResource(Res.string.change_password),
+                style = MaterialTheme.typography.headlineSmall
             )
-            OutlinedTextField(
-                value = changePasswordState.newPassword,
-                onValueChange = changePasswordState::updateNewPassword,
-                label = {
-                    Text(
-                        stringResource(Res.string.new_password),
-                        color = MaterialTheme.colorScheme.onSurface
-                    )
-                },
-                singleLine = true,
-                supportingText = { Text(stringResource(Res.string.password_must_be_at_least_8_characters_long)) },
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(horizontal = 8.dp, vertical = 4.dp)
-            )
-            OutlinedTextField(
-                value = changePasswordState.confirmPassword,
-                onValueChange = changePasswordState::updateConfirmPassword,
-                label = {
-                    Text(
-                        stringResource(Res.string.confirm_password),
-                        color = MaterialTheme.colorScheme.onSurface
-                    )
-                },
-                singleLine = true,
-                supportingText = {
-                    Text(
-                        stringResource(Res.string.passwords_must_match),
-                        color = MaterialTheme.colorScheme.onSurface) },
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(horizontal = 8.dp, vertical = 4.dp)
-            )
-            Button(
-                onClick = {},
+            Column(
                 modifier = Modifier.fillMaxWidth()
-            ) { Text(stringResource(Res.string.change_password))}
+                    .fillMaxHeight(0.6f),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.SpaceEvenly
+            ) {
+                OutlinedTextField(
+                    value = changePasswordState.currentPassword,
+                    onValueChange = changePasswordState::updateCurrentPassword,
+                    label = {
+                        Text(
+                            stringResource(Res.string.old_password),
+                            color = MaterialTheme.colorScheme.onSurface
+                        )
+                    },
+                    singleLine = true,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 8.dp, vertical = 4.dp)
+                )
+                OutlinedTextField(
+                    value = changePasswordState.newPassword,
+                    onValueChange = changePasswordState::updateNewPassword,
+                    label = {
+                        Text(
+                            stringResource(Res.string.new_password),
+                            color = MaterialTheme.colorScheme.onSurface
+                        )
+                    },
+                    singleLine = true,
+                    supportingText = { Text(stringResource(Res.string.password_must_be_at_least_8_characters_long)) },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 8.dp, vertical = 4.dp)
+                )
+                OutlinedTextField(
+                    value = changePasswordState.confirmPassword,
+                    onValueChange = changePasswordState::updateConfirmPassword,
+                    label = {
+                        Text(
+                            stringResource(Res.string.confirm_password),
+                            color = MaterialTheme.colorScheme.onSurface
+                        )
+                    },
+                    singleLine = true,
+                    supportingText = {
+                        Text(
+                            stringResource(Res.string.passwords_must_match),
+                            color = MaterialTheme.colorScheme.onSurface
+                        )
+                    },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 8.dp, vertical = 4.dp)
+                )
+                Button(
+                    onClick = {},
+                    modifier = Modifier.fillMaxWidth()
+                ) { Text(stringResource(Res.string.change_password)) }
+            }
         }
+        if (isSnackBarVisible) DismissibleSnackBar(isSnackBarVisible) { Text("Yes it goons.") }
     }
 }
--- composeApp/src/commonMain/kotlin/com/smokinggunstudio/vezerfonal/ui/screens/RequestAccountDeletionScreen.kt
+++ composeApp/src/commonMain/kotlin/com/smokinggunstudio/vezerfonal/ui/screens/RequestAccountDeletionScreen.kt
@@ -2,6 +2,7 @@ package com.smokinggunstudio.vezerfonal.ui.screens
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
@@ -17,11 +18,16 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import com.smokinggunstudio.vezerfonal.ui.components.DismissibleSnackBar
 import org.jetbrains.compose.resources.stringResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
 import vezerfonal.composeapp.generated.resources.Res
@@ -32,72 +38,78 @@ import vezerfonal.composeapp.generated.resources.request_account_deletion
 
 @Composable
 fun RequestAccountDeletionScreen() {
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(color = MaterialTheme.colorScheme.surface)
-            .padding(8.dp),
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
+    var isSnackBarVisible by remember { mutableStateOf(false) }
+    Box{
         Column(
             modifier = Modifier
-                .fillMaxWidth()
-                .fillMaxHeight(0.5f),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.SpaceEvenly
-        ){
-            Row(
-                modifier = Modifier.fillMaxWidth(),
-                verticalAlignment = Alignment.CenterVertically,
-                horizontalArrangement = Arrangement.Center
-            ) {
-                Text(
-                    text = stringResource(Res.string.request_account_deletion),
-                    style = MaterialTheme.typography.headlineLarge,
-                    textAlign = TextAlign.Center
-                )
-            }
-            Icon(
-                imageVector = Icons.Outlined.Delete,
-                contentDescription = null,
-                Modifier.size(128.dp)
-            )
-            Text(
-                text = stringResource(Res.string.account_deletion_warning),
-                style = MaterialTheme.typography.bodyMedium.copy(color = MaterialTheme.colorScheme.onSurface),
-                textAlign = TextAlign.Center,
-                fontWeight = FontWeight.Bold
-            )
-            Button(
-                onClick = {},
-                modifier = Modifier.fillMaxWidth()
+                .fillMaxSize()
+                .background(color = MaterialTheme.colorScheme.surface)
+                .padding(8.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .fillMaxHeight(0.5f),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.SpaceEvenly
             ) {
                 Row(
-                    verticalAlignment = Alignment.CenterVertically
+                    modifier = Modifier.fillMaxWidth(),
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.Center
                 ) {
-                    Icon(
-                        imageVector = Icons.Outlined.Cancel,
-                        contentDescription = null
-                    )
                     Text(
-                        text = stringResource(Res.string.cancel)
+                        text = stringResource(Res.string.request_account_deletion),
+                        style = MaterialTheme.typography.headlineLarge,
+                        textAlign = TextAlign.Center
                     )
                 }
-            }
-            Button(
-                onClick = {},
-                modifier = Modifier.fillMaxWidth()
-            ) {
-                Row(
-                    verticalAlignment = Alignment.CenterVertically
+                Icon(
+                    imageVector = Icons.Outlined.Delete,
+                    contentDescription = null,
+                    Modifier.size(128.dp)
+                )
+                Text(
+                    text = stringResource(Res.string.account_deletion_warning),
+                    style = MaterialTheme.typography.bodyMedium.copy(color = MaterialTheme.colorScheme.onSurface),
+                    textAlign = TextAlign.Center,
+                    fontWeight = FontWeight.Bold
+                )
+                Button(
+                    onClick = {},
+                    modifier = Modifier.fillMaxWidth()
                 ) {
-                    Icon(
-                        imageVector = Icons.Outlined.Delete,
-                        contentDescription = null)
-                    Text(
-                        text = stringResource(Res.string.delete_my_account))
+                    Row(
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        Icon(
+                            imageVector = Icons.Outlined.Cancel,
+                            contentDescription = null
+                        )
+                        Text(
+                            text = stringResource(Res.string.cancel)
+                        )
+                    }
+                }
+                Button(
+                    onClick = {},
+                    modifier = Modifier.fillMaxWidth()
+                ) {
+                    Row(
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        Icon(
+                            imageVector = Icons.Outlined.Delete,
+                            contentDescription = null
+                        )
+                        Text(
+                            text = stringResource(Res.string.delete_my_account)
+                        )
+                    }
                 }
             }
         }
+        if (isSnackBarVisible) DismissibleSnackBar(isSnackBarVisible) { Text("Yes it goons.")  }
     }
 }
