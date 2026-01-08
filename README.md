176ea5909637827b284319bd2594b9aae0d03cbe
murki05 <baloghmark05@gmail.com>
2026-01-08 13:04:23 +0100

    to-do list and documentation

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
 # Vezérfonal
+
+## 1. Áttekintés
+### Mit csinál ez az app?
+Appunk fő célja az, hogy a szervezeteken és iskolákon belüli kommunikációt fejlessze és finomhangolja. Az adminisztrátorok üzeneteket küldhetnek csoportoknak és egyéneknek, akik aztán reagálhatnak az üzenetekre a feladó által kiválasztott emojik valamelyikével.
+
+### Kiknek szól ez az app?
+
+- Cégek alkalmazottjainak
+- Iskolai személyzetnek, tanároknak, rendszergazdáknak
+- Akárkinek, aki cégen belül üzeneteket kap
+
+### Előnyök
+
+- Tiszta, céltudatos kommunikáció
+- Könnyű csoportalapú üzenetküldés
+- Gyors visszajelzés küldése és fogadása az emoji reakciók segítségével
+- Nincs irreleváns üzenetek által okozott rendetlenség
+
+## 2. Használat kezdése
+
+### Alkalmazás letöltése
+1. Töltsd le az alkalmazást az App Store-ról vagy Play Áruházból.
+2. Nyisd meg az alkalmazást.
+
+### Bejelentkezés
+1. Add meg a cég által biztosított regisztrációs kódot.
+2. Adj meg egy érvényes e-mail címet és egy jelszót.
+3. Adj meg egy profilképet.
--- composeApp/src/commonMain/kotlin/com/smokinggunstudio/vezerfonal/TODO.kt
+++ composeApp/src/commonMain/kotlin/com/smokinggunstudio/vezerfonal/TODO.kt
@@ -1,2 +1,23 @@
 package com.smokinggunstudio.vezerfonal
 
+fun main() {
+    TODO("group create crash")
+    TODO("sender emoji bug on iphone")
+    TODO("group, individual and tag select doesn't reopen when not closed via the cancel button")
+    TODO("swipeable group cards don't work")
+    TODO("create group popup text fields")
+    TODO("edit display name in account settings")
+    TODO("change pfp in account settings")
+    TODO("reveal id and email in account settings")
+    TODO("change password screen crashes when inputting a backspace in the code field(only when there is a single digit in the field)")
+    TODO("set up 2fa screen")
+    TODO("set up account deletion popup")
+    TODO("user and tag management screens are completely blank")
+    TODO("archive options screen")
+    TODO("notification settings screen")
+    TODO("TOS")
+    TODO("language selection screen")
+    TODO("bottom sheet shows up on the recipient's read message screen")
+    TODO("bottom sheet shows up on the recipient's archive screen")
+    TODO("keyboard blocks the bottom of the screen when writing a message (on iphone)")
+}
