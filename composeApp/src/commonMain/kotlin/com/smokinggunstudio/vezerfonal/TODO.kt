195b75691d13a1731af782fc16a87d8db3b3cefe
garon <21.gubacsi.aron.mate@mechwart.com>
2026-01-13 10:41:35 +0100

    todo listing

--- composeApp/src/commonMain/kotlin/com/smokinggunstudio/vezerfonal/TODO.kt
+++ composeApp/src/commonMain/kotlin/com/smokinggunstudio/vezerfonal/TODO.kt
@@ -1,6 +1,5 @@
 package com.smokinggunstudio.vezerfonal
 
-fun todo() {
 //    Critical:
 //    TODO: group edit and delete buttons dont work
 //    TODO: reg code edit button doesnt work
@@ -17,9 +16,7 @@ fun todo() {
 //    TODO: set up 2fa screen
 //    TODO: set up account deletion popup
 //    TODO: archive options screen
-}
 
-fun done() {
 //    Done: group create crash (unknown error)
 //    Done: group, individual and tag select doesn't reopen when not closed via the cancel button
 //    Done: swipeable group cards don't work
@@ -28,4 +25,3 @@ fun done() {
 //    Done: create group popup text fields
 //    Done: reveal id and email in account settings
 //    Done: change password screen crashes when inputting a backspace in the code field (only when there is a single digit in the field)
-}
