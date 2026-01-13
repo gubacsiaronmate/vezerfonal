3e1f9246314e20f99279871d03eff797d2a808e7
murki05 <baloghmark05@gmail.com>
2026-01-13 11:25:54 +0100

    asd

---
+++ composeApp/src/commonMain/kotlin/com/smokinggunstudio/vezerfonal/ui/components/RegCodeEditDialog.kt
@@ -0,0 +1,21 @@
+package com.smokinggunstudio.vezerfonal.ui.components
+
+import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun RegCodeEditDialog(
+    modifier: Modifier = Modifier
+) {
+    Dialog(
+        modifier = modifier
+    ) {
+        OutlinedTextField()
+        Button(
+            onClick = {},
+        ) {}
+        Button()
+    }
+}
