5ef371aaf732e46757df291c0337a1ff104f2db8
garon <21.gubacsi.aron.mate@mechwart.com>
2026-02-10 13:38:20 +0100

    asd

--- composeApp/src/commonMain/kotlin/com/smokinggunstudio/vezerfonal/network/helpers/PushNotificationText.kt
+++ composeApp/src/commonMain/kotlin/com/smokinggunstudio/vezerfonal/network/helpers/PushNotificationText.kt
@@ -10,7 +10,7 @@ val NotificationData.pushNotifTextRes: StringResource
         NotificationType.Message -> Res.string.sent_a_message
         NotificationType.Nudge -> Res.string.check_out_message
         NotificationType.Reaction ->
-            if (!data["reaction"].isNullOrEmpty())
+            if (!data["extra"].isNullOrEmpty())
                 Res.string.reacted_with
             else Res.string.has_marked_as_read
     }
